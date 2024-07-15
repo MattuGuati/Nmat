f032f64b8770ca19eaff6d506bc1bacc94c20570
Matteo <matteopeirano185@gmail.com>
2024-07-15 17:00:06 -0300

    2 commit

--- app.py
+++ app.py
@@ -8,20 +8,19 @@ from email_alerts import enviar_alerta
 
 app = dash.Dash(__name__)
 
-# Rango de IP predefinido y empresa
-IP_RANGE = '10.72.2.0-8'
+IP_RANGE = '10.72.2.0-9'
 EMPRESA_NOMBRE = 'Mi Red'
 
 app.layout = html.Div([
     html.H1(f"Escaneo de Red - {EMPRESA_NOMBRE}"),
     html.H2(f"Rango de IP: {IP_RANGE}"),
-    html.Button('Iniciar Escaneo', id='scan-button'),
-    html.Div(className='loader', id='loading-indicator'),  # Indicador de carga
-    html.Div(id='alert-container'),
+    html.Button('Iniciar Escaneo', id='scan-button', className="btn btn-primary"),
+    html.Div(id='alert-container', className="alert alert-info"),
     dcc.Graph(id='network-status'),
     html.Div(id='network-table-container', className='dash-table-container'),
     dcc.Graph(id='port-status'),
-    html.Div(id='port-table-container', className='dash-table-container')
+    html.Div(id='port-table-container', className='dash-table-container'),
+    html.Div(className='loader mx-auto', id='loading-indicator', style={'display': 'none'})
 ])
 
 @app.callback(
@@ -30,37 +29,36 @@ app.layout = html.Div([
      Output('network-table-container', 'children'),
      Output('port-table-container', 'children'),
      Output('alert-container', 'children'),
-     Output('loading-indicator', 'style')],
+     Output('loading-indicator', 'style'),
+     Output('alert-container', 'style')],
     [Input('scan-button', 'n_clicks')]
 )
 def scan_network(n_clicks):
     if n_clicks is None:
-        return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, {'display': 'none'}
+        return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, {'display': 'none'}, {'display': 'none'}
 
     print("Iniciando escaneo...")
 
-    # Mostrar el indicador de carga
     loading_style = {'display': 'block'}
+    alert_style = {'display': 'none'}
 
     scan_results_df = realizar_escaneo(IP_RANGE)
     if scan_results_df.empty:
         print("No se encontraron resultados en el escaneo de red.")
-        return dash.no_update, dash.no_update, "No se encontraron resultados en el escaneo de red.", dash.no_update, dash.no_update, {'display': 'none'}
+        return dash.no_update, dash.no_update, "No se encontraron resultados en el escaneo de red.", dash.no_update, dash.no_update, {'display': 'none'}, {'display': 'block'}
 
     port_results_df = realizar_escaneo_puertos(scan_results_df['ip'].tolist())
     if port_results_df.empty:
         print("No se encontraron resultados en el escaneo de puertos.")
-        return dash.no_update, dash.no_update, dash.no_update, "No se encontraron resultados en el escaneo de puertos.", dash.no_update, {'display': 'none'}
+        return dash.no_update, dash.no_update, dash.no_update, "No se encontraron resultados en el escaneo de puertos.", dash.no_update, {'display': 'none'}, {'display': 'block'}
 
     print("Escaneo completado. Comparando resultados...")
 
-    # Comparar con resultados anteriores
-    scan_results_previous_df = pd.read_csv('scan_results.csv', dtype={'ip': str})
-    port_results_previous_df = pd.read_csv('port_results.csv', dtype={'ip': str})
+    scan_results_previous_df = pd.read_csv('scan_results.csv')
+    port_results_previous_df = pd.read_csv('port_results.csv')
 
     alerts = []
 
-    # Detectar cambios en las IPs
     combined_df = pd.merge(scan_results_df, scan_results_previous_df, on='ip', how='outer', suffixes=('', '_previous'))
     combined_df['estado_previous'].fillna('down', inplace=True)
     combined_df['estado'].fillna('down', inplace=True)
@@ -72,7 +70,6 @@ def scan_network(n_clicks):
         elif row['estado'] == 'down':
             alerts.append(f"La IP {row['ip']} ha cambiado de estado a down")
 
-    # Detectar cambios en los puertos
     combined_ports_df = pd.merge(port_results_df, port_results_previous_df, on=['ip', 'puerto'], how='outer', suffixes=('', '_previous'))
     combined_ports_df['estado_previous'].fillna('closed', inplace=True)
     combined_ports_df['estado'].fillna('closed', inplace=True)
@@ -84,17 +81,17 @@ def scan_network(n_clicks):
         elif row['estado'] == 'closed':
             alerts.append(f"El puerto {row['puerto']} en la IP {row['ip']} ha cambiado de estado a closed")
 
-    # Guardar los resultados actuales como históricos
     scan_results_df.to_csv('scan_results.csv', index=False)
     port_results_df.to_csv('port_results.csv', index=False)
 
     if alerts:
-        alert_message = "\n".join(alerts)
-        enviar_alerta(alert_message)
+        alert_message = html.Ul([html.Li(alert) for alert in alerts])
+        enviar_alerta(" | ".join(alerts))
+        alert_style = {'display': 'block'}
     else:
         alert_message = "No se encontraron cambios en el estado de las IPs y puertos."
+        alert_style = {'display': 'block'}
 
-    # Ocultar el indicador de carga
     loading_style = {'display': 'none'}
 
     network_fig = px.pie(scan_results_df, names='estado', title='Estado de los Dispositivos en la Red')
@@ -112,7 +109,7 @@ def scan_network(n_clicks):
         style_table={'height': '300px', 'overflowY': 'auto'}
     )
 
-    return network_fig, port_fig, network_table, port_table, alert_message, loading_style
+    return network_fig, port_fig, network_table, port_table, alert_message, loading_style, alert_style
 
 if __name__ == '__main__':
     app.run_server(debug=True)
